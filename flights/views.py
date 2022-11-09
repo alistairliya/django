eb87780b19a329ddba9ded977024809051bf0b9b
Eugene Lin <eugenelin89@gmail.com>
2022-11-09 13:08:05 -0800

    flight page for each fligh in flights model

--- flights/views.py
+++ flights/views.py
@@ -6,3 +6,9 @@ def index(request):
     return render(request, "flights/index.html",{
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id) # can also use id in place of pk
+    return render(request,"flights/flight.html",{
+        "flight":flight
+    } )
