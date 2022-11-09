94bbd91678d99b9258c835c87e70566ec43eb1b0
Eugene Lin <eugenelin89@gmail.com>
2022-11-09 14:01:46 -0800

    Still working on POST to add passenger. 1:33:32

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight
+from .models import Flight, Passenger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -10,5 +12,14 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id) # can also use id in place of pk
     return render(request,"flights/flight.html",{
-        "flight":flight
+        "flight":flight,
+        'passengers':flight.passengers.all()
     } )
+
+def book(request, flight_id):
+    if request.method == "POST":
+        # Need some error checking here.
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk = int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flights", args={flight.id}))
