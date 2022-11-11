45842654351d0b49f3e2f0a63c2916b3a18b3d52
Eugene Lin <eugenelin89@gmail.com>
2022-11-11 15:09:10 -0800

    Can now redirect back to page after login.

--- business/views.py
+++ business/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, "business/index.html")
 
