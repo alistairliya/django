bc25e42a967b421033aafa084417fcaeb3f42167
Eugene Lin <eugenelin89@gmail.com>
2022-11-10 18:07:15 -0800

    Started My Business App

--- business/views.py
+++ business/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "business/index.html")
+
+def new_business(request):
+    return render(request, "business/new_business.html")
