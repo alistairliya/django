a63749da8abc30749b3251b6fc66d656f62f5b7d
Eugene Lin <eugenelin89@gmail.com>
2022-11-15 14:55:55 -0800

    Begin to code model for Business

--- business/models.py
+++ business/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+# Referencing User Model:
+# https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#referencing-the-user-model
+# https://learndjango.com/tutorials/django-best-practices-referencing-user-model
+# Custom User Model:
+# https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+# https://learndjango.com/tutorials/django-custom-user-model
+
 # Create your models here.
+class MyBusiness(models.Model):
+    pass
+
+class Business_User(models.Model):
+    pass
