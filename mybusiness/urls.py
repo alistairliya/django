18bd4c074313b543becf7ce74d66fc9bd0ea34e8
Eugene Lin <eugenelin89@gmail.com>
2022-11-15 16:43:46 -0800

    Adding mybusiness app, intended to replace business app.

---
+++ mybusiness/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "mybusiness"
+urlpatterns = [
+
+    path("", views.index, name="index"),
+    path("new_business", views.new_business, name="new_business"),
+   
+]
