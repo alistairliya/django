745ec85b0b0d3a416aab23e0f4faf1a07464432d
Eugene Lin <eugenelin89@gmail.com>
2022-11-22 17:51:59 -0800

    comments

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -1,3 +1,7 @@
+# https://www.digitalocean.com/community/tutorials/build-a-to-do-application-using-django-and-react
+# https://www.django-rest-framework.org/
+# https://www.django-rest-framework.org/tutorial/quickstart/
+
 from rest_framework import serializers
 from mybusiness.models import *
 
