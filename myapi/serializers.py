b3f1717dd7998373e0e6e85d32421c5b6b58ffab
Eugene Lin <eugenelin89@gmail.com>
2022-12-01 11:54:38 +0800

    comment

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -34,6 +34,20 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 #     created_by = PrimaryKeyRelatedField(queryset=MyUser.objects.all())
 #     created_date = DateTimeField()
 #     modified_date = DateTimeField()
+# >>> male = Gender(gender_name ='Male',description='Not a girl',gender_code='M')
+# >>> male.save()
+# >>> female = Gender(gender_name ='Female',description='Not a boy',gender_code='F')
+# >>> female.sve()
+# >>> me = MyUser.objects.all()[0]
+# >>> foo = Client(first_name='Foo', middle_name='Dan', last_name='Bar', birthdate=parse_date('2022-12-01'), gender=male, created_by=me, created_date=parse_date('2022-12-01'), modified_date=parse_date('2022-12-01'))
+# >>> foo.save()
+# >>> from mybusiness.models import Client
+# >>> from myapi.serializers import ClientSerializer
+# >>> c = Client.objects.all()[0]
+# >>> s = ClientSerializer(c)
+# >>> s.data
+# {'id': 1, 'first_name': 'Foo', 'middle_name': 'Dan', 'birthdate': '2022-12-01', 'last_name': 'Bar', 'sin': '', 'gender': 1, 'created_by': 1, 'created_date': '2022-12-01T00:00:00Z', 'modified_date': '2022-12-01T00:00:00Z'}
+# >>> 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
