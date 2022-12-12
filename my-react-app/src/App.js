ba636f1c4a0bc672e16cb80659b862f249395cf2
Eugene Lin <eugenelin89@gmail.com>
2022-12-12 16:31:51 +0800

    refactor by lifting hardcoded businesses to app

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,45 @@
+import {useState} from 'react'
 import Header from './components/Header'
 import Businesses from './components/Businesses'
 
 function App() {
+
+
+  const [businesses, setBusinesses] = useState(
+    [
+      {
+        "id": 1,
+        "business_type": "http://127.0.0.1:8000/api/businesstype/1/",
+        "product": "http://127.0.0.1:8000/api/product/1/",
+        "client": "http://127.0.0.1:8000/api/clients/1/",
+        "status": "http://127.0.0.1:8000/api/businessstatus/1/",
+        "projected_FYC": 1.0,
+        "application_date": "2022-12-09",
+        "application_location": "Kaohsiung",
+        "created_by": "eugene",
+        "created_date": "2022-12-09T13:22:00Z",
+        "modified_date": "2022-12-09T13:22:00Z"
+      },
+      {
+        "id": 2,
+        "business_type": "http://127.0.0.1:8000/api/businesstype/1/",
+        "product": "http://127.0.0.1:8000/api/product/1/",
+        "client": "http://127.0.0.1:8000/api/clients/1/",
+        "status": "http://127.0.0.1:8000/api/businessstatus/1/",
+        "projected_FYC": 2.0,
+        "application_date": "2022-12-12",
+        "application_location": "Vancouver",
+        "created_by": "eugene",
+        "created_date": "2022-12-12T15:52:00Z",
+        "modified_date": "2022-12-12T15:52:00Z"
+      }
+    ]
+  )
+
   return (
     <div className="container">
       <Header title='My Businesses'/>
-      <Businesses/>
+      <Businesses businesses = {businesses}/>
     </div>
   );
 }
