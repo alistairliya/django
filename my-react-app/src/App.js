91e87204b53a21183a7807d05dfde2d0b2224303
Eugene Lin <eugenelin89@gmail.com>
2022-12-12 16:26:48 +0800

    working on state

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Businesses from './components/Businesses'
 
 function App() {
   return (
     <div className="container">
       <Header title='My Businesses'/>
+      <Businesses/>
     </div>
   );
 }
