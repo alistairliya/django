1f154438ccb14cd4b87271e43b3bebf8ab4b90f4
Eugene Lin <eugenelin89@gmail.com>
2022-12-14 10:33:18 +0800

    component state in the form

--- my-react-app/src/components/NewBusiness.js
+++ my-react-app/src/components/NewBusiness.js
@@ -1,23 +1,30 @@
 // Modeled AddTask example, 1:03:25
+import { useState } from 'react'
+
 
 const NewBusiness = () => {
-  return (
+    const [lastName, setLastName] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [address, setAddress] = useState('')
+    const [submitted, setSumbmitted] = useState(false)
+
+    return (
     <form className="add-form">
         <div className="form-control">
             <label>Last Name</label>
-            <input type='text' placeholder="Client's Last Name" />
+            <input type='text' placeholder="Client's Last Name" value={lastName} onChange={(e)=>setLastName(e.target.value)} />
         </div>
         <div className="form-control">
             <label>First Name</label>
-            <input type='text' placeholder="Client's First Name" />
+            <input type='text' placeholder="Client's First Name" value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
         </div>
         <div className="form-control">
             <label>Address</label>
-            <input type='text' placeholder="Client's Address" />
+            <input type='text' placeholder="Client's Address" value={address} onChange={(e)=>setAddress(e.target.value)}/>
         </div>
         <div className='form-control form-control-check'>
             <label>Document Submitted</label>
-            <input type='checkbox' />
+            <input type='checkbox' value={submitted} onChange={(e)=>setSumbmitted(e.currentTarget.checked)}/>
         </div>
         <input type='submit' value='Add Business' className='btn btn-block' />
     </form>
