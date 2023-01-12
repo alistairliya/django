9b10036e3d3352773d6dd4ee887c98cc2f98c4be
Eugene Lin <eugenelin89@gmail.com>
2023-01-12 14:21:52 -0800

    put a table around Business component

--- my-react-app/src/components/Business.js
+++ my-react-app/src/components/Business.js
@@ -2,16 +2,30 @@
 // 47.07
 import React from 'react'
 import {AiFillTool} from "react-icons/ai" //https://react-icons.github.io/react-icons
-
+/*
 const Business = ({business, onEdit, onToggle}) => {
   return (
     <div className={`business ${business.highlighted? 'highlighted':''}`} onDoubleClick={()=>onToggle(business.id)}>
-        <h3>
-            {business.id}<AiFillTool style={{color:'red', cursor:'pointer'}} onClick={()=>onEdit(business.id)}/>
-        </h3>
-        <p>{business.product}</p>       
+        <td>
+            {business.id}
+        </td>
+        <td>{business.product}</td>
+        <td><AiFillTool style={{color:'red', cursor:'pointer'}} onClick={()=>onEdit(business.id)}/></td>       
     </div>
   )
 }
-
+*/
+const Business = ({business, onEdit, onToggle}) => {
+  return (
+    <tbody className={`business ${business.highlighted? 'highlighted':''}`} onDoubleClick={()=>onToggle(business.id)}>
+    <tr >
+        <td>
+            {business.id}
+        </td>
+        <td>{business.product}</td>
+        <td><AiFillTool style={{color:'red', cursor:'pointer'}} onClick={()=>onEdit(business.id)}/></td>       
+  </tr>
+  </tbody>
+  )
+}
 export default Business
--- my-react-app/src/components/Businesses.js
+++ my-react-app/src/components/Businesses.js
@@ -4,11 +4,18 @@ import Business from './Business'
 
 const Businesses = ({businesses, onEdit, onToggle}) => {
   return (
-    <>
+    <table>
+      <tbody className='business'>
+      <tr align="center" border="1px solid black">
+        <td>Application Date</td>
+        <td>Settled Date</td>
+        <td>Trans ID</td>
+     </tr>
+     </tbody>
       {businesses.map((business)=>(
         <Business key={business.id} business={business} onEdit={onEdit} onToggle={onToggle}/>
       ))}
-    </>
+    </table>
   )
 }
 
