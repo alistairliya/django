ef9c81c2e3564030220b73afe4520a9a095dbec8
Eugene Lin <eugenelin89@gmail.com>
2023-01-28 14:07:02 -0800

    getting country list

--- my-react-app/src/components/NewAddress.js
+++ my-react-app/src/components/NewAddress.js
@@ -1,7 +1,66 @@
+import { useState, useEffect } from "react"
+import { useAuth } from "../hooks/useAuth"
 
-const NewAddress = () => {
+const NewAddress = ({setAddress}) => {
+    const {user} = useAuth()
+    const [unitNumber, setUnitNumber] = useState()
+    const [streetAddress, setStreetAddress] = useState()
+    const [city, setCity] = useState()
+    const [postalCode, setPostalCode] = useState() 
+    const [countryList, setCountryList] = useState()
+
+    const onSubmit = (e) =>{
+        setAddress({
+            unit_number:unitNumber,
+            street_address: streetAddress,
+            city: city,
+            postal_code: postalCode,
+        })
+    }
+
+    const fetchCountryList = async()=>{
+        console.log('start fetchCountryList')
+        let headers = new Headers()
+        const token = user['token']
+        const auth_str = 'Token '+token
+        headers.set('Authorization', auth_str)
+        let url = 'http://localhost:8000/api/country/'
+        const res = await fetch(url, {headers:headers})
+        const data = await res.json()
+        console.log('done fetchCountryList')
+        return data
+    }
+
+    useEffect(()=>{
+        const getCountryList = async () =>{
+            const theCountryList = await fetchCountryList()
+            console.log("The Country List:")
+            console.log(theCountryList)
+            setCountryList(theCountryList)
+        }
+        getCountryList()
+        console.log('Countries:')
+        console.log(countryList) 
+    },[])
+
   return (
-    <div>NewAddress</div>
+    <div>
+    <form className="add-form" onSubmit={onSubmit}>
+        <div className="form-control">
+            <label>Unit Number:</label>
+            <input type='text' placeholder="Unit Number" value={unitNumber} onChange={(e)=> setUnitNumber(e.target.value)} />
+        </div>
+        <div className="form-control">
+            <label>Street Address</label>
+            <input type='text' placeholder="Street Address" value={streetAddress} onChange={(e)=>setStreetAddress(e.target.value)} />
+        </div>
+        <div className="form-control">
+            <label>City</label>
+            <input type='text' placeholder="City" value={city} onChange={(e)=>setCity(e.target.value)} />
+        </div>
+        <input type='submit' value='Next' className='btn btn-block' />
+    </form>
+    </div>
   )
 }
 
