21b18ba6eec238e9ca767ac29593d8ef3802681a
Eugene Lin <eugenelin89@gmail.com>
2023-02-02 15:47:27 -0800

    function to add another phone element git push git push

--- my-react-app/src/components/NBF4Phones.js
+++ my-react-app/src/components/NBF4Phones.js
@@ -10,7 +10,13 @@ const Phones = ({setPhones, existingPhones}) => {
         setApplicationPhones(applicationPhones.append(phone))
     }
 
+
+    const addAnotherPhone = ()=>{
+        setPhoneElementList(phoneElementList + (<Phone addPhone = {addPhone} existingPhones = {existingPhones}/>))
+    }
+
     useEffect(()=>{
+        //addAnotherPhone() // cant do this because useEffect is called after the UI is rendered. phoneElementList need to be already constructed.
         setPhoneElementList([<Phone addPhone = {addPhone} existingPhones = {existingPhones}/>])
     },[])
 
