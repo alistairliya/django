484acf051e0e57d6731ab6d3729027a24f0b2bfd
Eugene Lin <eugenelin89@gmail.com>
2023-02-02 17:59:02 -0800

    can add additional phones

--- my-react-app/src/components/NBF4Phones.js
+++ my-react-app/src/components/NBF4Phones.js
@@ -1,34 +1,43 @@
 import Phone from './NBF4PhonesPhone.js'
 import {useState, useEffect} from 'react'
+import Button from './Button'
 
 const Phones = ({setPhones, existingPhones}) => {
     const [applicationPhones, setApplicationPhones] = useState([])  
     const [phoneElementList, setPhoneElementList] = useState([])
 //<Phone addPhone = {addPhone} existingPhones = {existingPhones}/>
-
+    const [key, setKey] = useState(0)
     const addPhone = (phone) => {
         setApplicationPhones(applicationPhones.append(phone))
     }
 
 
     const addAnotherPhone = ()=>{
-        setPhoneElementList(phoneElementList + (<Phone addPhone = {addPhone} existingPhones = {existingPhones}/>))
+        console.log(phoneElementList)
+        setPhoneElementList([...phoneElementList, <Phone key = {key.toString()} addPhone = {addPhone} existingPhones = {existingPhones}/>])
+        setKey(key+1)
     }
 
     useEffect(()=>{
         //addAnotherPhone() // cant do this because useEffect is called after the UI is rendered. phoneElementList need to be already constructed.
-        setPhoneElementList([<Phone addPhone = {addPhone} existingPhones = {existingPhones}/>])
+        setPhoneElementList([<Phone key='x' addPhone = {addPhone} existingPhones = {existingPhones}/>])
     },[])
-
 
 
     return (
     <div>
-        {phoneElementList.map(
+        {
+        phoneElementList.map(
             (phoneElement)=>{
                 return phoneElement
             }
-        )}
+        )
+        }
+            <Button 
+                text='Add another' 
+                color='steelblue' 
+                onClick={addAnotherPhone} 
+            />
     </div>
   )
 }
