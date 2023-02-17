3fea2c6bee4f050052014e930f3eab87b0e573b5
Eugene Lin <eugenelin89@gmail.com>
2023-02-17 12:01:49 -0800

    Putting Phone element in state variable

--- my-react-app/src/components/NBF4.js
+++ my-react-app/src/components/NBF4.js
@@ -4,19 +4,23 @@ import {useState, useEffect} from 'react'
 const NBF4 = ({setApplicantContacts, client}) => {
     const [phones, setPhones] = useState({})
     const [emails, setEmails] = useState({})
+    const [phonesElement, setPhonesElement] = useState([<Phones setPhones = {setPhones} existingPhones = {client.phone_list}/>])
+
     useEffect(()=>{
         console.log(client)
-    })
+    },[])
+
     const onSubmit = (e) =>{
         e.preventDefault()
         console.log('Next pressed')
+        console.log(phonesElement[0]['phoneElementList'])
     }
 
     return (
     <div>
       <h2>New Business Form - Client Phone Contacts</h2>
-      <div>      
-      <Phones setPhones = {setPhones} existingPhones = {client.phone_list}/>
+      <div>
+        {phonesElement[0]}      
       </div>
 
       <form className="add-form" onSubmit={onSubmit}>
