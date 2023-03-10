024d9568f9fc4c6c8d8e1a96db24a0694250cae8
Eugene Lin <eugenelin89@gmail.com>
2023-03-10 12:17:04 -0800

    fetch medicals

--- my-react-app/src/components/NBF6.js
+++ my-react-app/src/components/NBF6.js
@@ -1,7 +1,31 @@
 // Order Medical
 // List of medical exams, 
+
+import { useEffect,useState } from "react"
+import { useAuth } from "../hooks/useAuth"
+
 // Select ones that apply
 const NBF6 = () => {
+    const { user } = useAuth()
+    const [availableMedicals, setAvailableMedicals] = useState([])
+    useEffect(()=>{
+        console.log('NBF6 useEffect')
+        // Get the available medical exams
+        const fetchResource = async (resource) =>{
+            let headers = new Headers()
+            const token = user['token']
+            const auth_str = 'Token '+token
+            headers.set('Authorization', auth_str)
+            let url = 'http://localhost:8000/api/'+resource+'/'
+            const res = await fetch(url,{headers:headers})
+            const data = await res.json()
+            setAvailableMedicals(data)
+            console.log(data)
+            return data
+        }
+        fetchResource('medical')
+    },[])
+
   return (
     <div>NBF6</div>
   )
