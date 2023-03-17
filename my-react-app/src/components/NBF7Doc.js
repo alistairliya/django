085fbf118b857033b290516a74716fa1a6161dc2
Eugene Lin <eugenelin89@gmail.com>
2023-03-17 04:20:52 -0700

    add comments

--- my-react-app/src/components/NBF7Doc.js
+++ my-react-app/src/components/NBF7Doc.js
@@ -1,3 +1,8 @@
+// Purpose: Display a single document from the NBF7 collection
+// checkbox for submitted
+// textbox for notes, such as where the doc is stored.
+// allows user to upload a document
+
 
 const NBF7Doc = ({id, item, collect}) => {
   return (
