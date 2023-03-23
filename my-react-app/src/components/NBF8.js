3a431f720ff1a150420632239e691547a194d2e1
Eugene Lin <eugenelin89@gmail.com>
2023-03-23 15:24:56 -0700

    getting users and roles. roles still need to be implemented in the back end

--- my-react-app/src/components/NBF8.js
+++ my-react-app/src/components/NBF8.js
@@ -1,10 +1,45 @@
 // Advisors Information
+// Get users
+// Get roles
+// Component to add users/roles
 
+import {useState, useEffect} from 'react'
+import {useAuth} from '../hooks/useAuth'
 
-const NBF8 = () => {
-  return (
-    <div>NBF8</div>
-  )
+const NBF8 = ({onNextClicked}) => {
+    const {user} = useAuth()
+    const {users, setUsers} = useState([])
+    const {roles, setRoles} = useState([])
+    useEffect(
+        ()=>{
+            console.log('useEffect in NBF8')
+            const fetchResource = async(resource)=>{
+                let headers = new Headers()
+                const token = user['token']
+                const auth_str = 'Token '+token
+                headers.set('Authorization', auth_str)
+                const res = await fetch('http://localhost:8000/api/'+resource+'/', {headers:headers})
+                const data = await res.json()
+                return data
+            }
+            if(users.length === 0 && roles.length === 0){
+                const getUsers = async ()=>{
+                    const theUsers = await fetchResource('user')
+                    setUsers(theUsers)
+                }
+                const getRoles = async ()=>{
+                    const theRoles = await fetchResource('role')
+                    setRoles(theRoles)
+                }
+                getUsers()
+                getRoles()
+            }
+
+        },[users, roles]
+    )    
+    return (
+        <div>NBF8</div>
+    )
 }
 
 export default NBF8
