315d081f703898582f1baa0db3aecd0b6ab4abdc
Eugene Lin <eugenelin89@gmail.com>
2023-04-26 11:09:00 -0700

    Fetching Client for BusinessDetails

--- my-react-app/src/components/BusinessDetails.js
+++ my-react-app/src/components/BusinessDetails.js
@@ -1,23 +1,56 @@
+// Data from:
+// Client <- WORKING ON FETCHING
+// MyBusiness <- already passed down by caller
+// Address
+// InsuranceApplication
+// BusinessUser
+// BusinessCompliance
+// BusinessDocument
+// BusinessMedical
+
+
 import Button from './Button'
 
 import {useEffect, useState} from "react"
+import { useAuth } from "../hooks/useAuth"
+
 
 const BusinessDetails = ({business, closeComponent}) => {
-useEffect(()=>{
-    console.log('BusinessDetails useEffect')
-    console.log(business)
-}, [business])
+    const [myClient, setMyClient] = useState({})
+    
+    const { user } = useAuth()
+    useEffect(()=>{
 
-  return (
-    <div className="container">
-    <div>BusinessDetails {business.id}</div>
-    <Button 
-    text='Close' 
-    color='red' 
-    onClick={closeComponent} 
-    />
-    </div>
-  )
+        console.log('BusinessDetails useEffect')
+        console.log(business.client)
+        const getMyClient = async () => {
+            let c = await fetchObject(business.client)
+            await setMyClient(c)
+            console.log("My Clinet: "+JSON.stringify(myClient))
+        }
+        getMyClient()
+    }, [business])
+
+    const fetchObject = async (url) =>{
+        let headers = new Headers()
+        const token = user['token']
+        const auth_str = 'Token '+token
+        headers.set('Authorization', auth_str)
+        const res = await fetch(url,{headers:headers})
+        const data = await res.json()
+        return data
+    }
+
+    return (
+        <div className="container">
+        <div>BusinessDetails {business.id}</div>
+        <Button 
+        text='Close' 
+        color='red' 
+        onClick={closeComponent} 
+        />
+        </div>
+    )
 }
 
 export default BusinessDetails
