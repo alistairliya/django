37b78332e83c6e7a6bb5a6ea1ef26082556cf7dd
Eugene Lin <eugenelin89@gmail.com>
2023-05-06 10:16:58 -0700

    still working on setting myInsurance

--- my-react-app/src/components/BusinessDetailsInsurance.js
+++ my-react-app/src/components/BusinessDetailsInsurance.js
@@ -5,10 +5,33 @@ import { useAuth } from "../hooks/useAuth"
 
 const BusinessDetailsInsurance = ({insurance}) => {
     const { user } = useAuth()
+    const [myInsurance, setMyInsurance] = useState(null)
     useEffect(()=>{
         console.log('BusinessDetailsInsurance useEffect()')
-        console.log(insurance)
+        //console.log(insurance)
+        const getPlan = async () => {
+            console.log('inside getPlan')
+            const p = await fetchObject(insurance.plan)
+            const t = await fetchObject(insurance.plan_type)
+            //console.log("got plan")
+            console.log(insurance)
+            setMyInsurance(insurance)
+
+        }
+        getPlan()
+        console.log("myInsurance...")
+        console.log(myInsurance)
     }, [insurance])
+
+    const fetchObject = async (url) =>{
+        let headers = new Headers()
+        const token = user['token']
+        const auth_str = 'Token '+token
+        headers.set('Authorization', auth_str)
+        const res = await fetch(url,{headers:headers})
+        const data = await res.json()
+        return data
+    }
 
     return (
         <div className="container">
