0cd7a91e3efe17cd492add54c74907ca47b39b01
Eugene Lin <eugenelin89@gmail.com>
2023-05-08 09:40:36 -0700

    reverse the order of mybusinesses

--- my-react-app/src/pages/MyBusinesses.js
+++ my-react-app/src/pages/MyBusinesses.js
@@ -51,7 +51,7 @@ function MyBusinesses() {
   useEffect(()=>{
     const getBusinesses = async() => {
       const businessesFromServer = await fetchBusiness()
-      setBusinesses(businessesFromServer)      
+      setBusinesses(businessesFromServer.reverse())      
     }
     getBusinesses()
   }, [])
