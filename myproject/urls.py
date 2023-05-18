1bfaf823d8fe1375c9919218b1f3f9bd6af49d30
Eugene Lin <eugenelin89@gmail.com>
2023-05-18 14:54:15 -0700

    removing test

--- myproject/urls.py
+++ myproject/urls.py
@@ -77,7 +77,6 @@ urlpatterns = [
     path('mybusiness/', include("mybusiness.urls")), # To replace business
     path('api/',include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('test/today/', views.test)
 ]
 from rest_framework.authtoken import views
 urlpatterns += [
