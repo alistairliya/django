46f7d3efb272f4ccf13021b18dd4ba89a122eaf4
Eugene Lin <eugenelin89@gmail.com>
2023-06-24 01:49:30 -0700

    NBF2 all saved when Prev clicked from NBF3

--- my-react-app/src/components/NewClient.js
+++ my-react-app/src/components/NewClient.js
@@ -6,24 +6,45 @@ const NewClient = ({client, onNextClicked, onPrevClicked, setClient, disabled}) 
 
   const [lastName, setLastName] = useState(client.search_last_name)
   const [firstName, setFirstName] = useState(client.search_first_name)
-  const [middleName, setMiddleName] = useState('')
-  const [birthDate, setBirthDate] = useState("2000-01-01")
-  const [sin, setSin] = useState('')
-  const [gender, setGender] = useState()
+  const [middleName, setMiddleName] = useState(client.middle_name)
+  const [birthDate, setBirthDate] = useState(client.birthdate)
+  const [sin, setSin] = useState(client.sin)
+  const [gender, setGender] = useState(client.gender)
+  const [selectedGender, setSelectedGender] = useState()
 
   useEffect(()=>{
     console.log('NewClient.js')
     console.log('NewClinet onNextClicked:')
     console.log(onNextClicked)
     console.log(client)
+    console.log("selected gender "+ client.gender)
     //console.log(onNextClicked)
     //console.log(setClient)
-  })
+    if(client.gender && client.gender === 'M'){
+      setSelectedGender({value:'M', label:'Male'})
+    }
+    if(client.gender && client.gender === 'F'){
+      setSelectedGender({value:'F', label:'Female'})
+    }
+    //console.log(selectedGender)
+
+  },[])
 
   const onSubmit = (e) =>{
       e.preventDefault() // avoiding submitting to a page.
       setClient({last_name:lastName, first_name:firstName, middle_name:middleName, gender:gender, sin:sin, birthdate:birthDate, is_new_client:true, search_first_name:client.search_first_name, search_last_name:client.search_last_name})
       onNextClicked()
+  }
+
+  const handleChange=(e)=>{
+    console.log(e)
+    setGender(e.value)
+    if(e.value === 'M')
+        setSelectedGender({value:'M', label:'Male'})
+    if(e.value === 'F')
+        setSelectedGender({value:'F', label:'Female'})
+
+
   }
 
   const previousClicked = (e) =>{
@@ -55,7 +76,7 @@ const NewClient = ({client, onNextClicked, onPrevClicked, setClient, disabled}) 
         </div>
         <div className="form-control">
         <label>Gender</label>
-          <Select options={[{value:'M',label:'Male'},{value:'F', label:'Female'}]} onChange={(e)=>{setGender(e.value)}} />
+          <Select value={selectedGender} options={[{value:'M',label:'Male'},{value:'F', label:'Female'}]} onChange={handleChange} />
         </div>
         <input type='submit' value='Prev' className='btn btn-block' onClick={previousClicked} />
         <input type='submit' value='Next' className='btn btn-block-2' />
