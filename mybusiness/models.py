cdf21ba545b89a8d5a4107c6c8385e0fd1388b0d
Eugene Lin <eugenelin89@gmail.com>
2023-07-13 16:35:19 -0700

    fixed applicant_client_address

--- mybusiness/models.py
+++ mybusiness/models.py
@@ -146,7 +146,7 @@ class MyBusiness(models.Model):
     application_date = models.DateField(null=True)
     settled_date = models.DateField(null=True)
     application_location = models.CharField(max_length=64, null=True)
-    applicant_client_address = models.ForeignKey(ClientAddress, on_delete=models.PROTECT, related_name="mybusinesses", null=True)
+    applicant_client_address = models.ForeignKey(Address, on_delete=models.PROTECT, related_name="mybusinesses", null=True)
     applicant_client_phone = models.ForeignKey(Phone, on_delete=models.PROTECT, related_name="mybusinesses", null=True)
     # Owner Datetime
     # The related name in created_by, my_businesses, corresponds to the my_businesses field in UserSerializer
