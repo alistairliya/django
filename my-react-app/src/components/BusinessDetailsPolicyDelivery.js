3236e12571ec59bad4436ae2aabd35f365b3a476
Eugene Lin <eugenelin89@gmail.com>
2023-09-15 23:56:06 -0700

    Submiting for review

--- my-react-app/src/components/BusinessDetailsPolicyDelivery.js
+++ my-react-app/src/components/BusinessDetailsPolicyDelivery.js
@@ -1,11 +1,53 @@
 //rafce
 
 import BusinessDetailsFP from './BusinessDetailsFP'
+import Button from './Button'
+import { useAuth } from "../hooks/useAuth"
 
 const BusinessDetailsPolicyDelivery = ({business, refreshBusinesses, hasWriteAccess = true}) => {
-  return (
+  
+    const { user } = useAuth()
+    
+    const submitForReview = async () =>{
+        if(!hasWriteAccess){
+            alert('Only the onwer or supervisor can SUBMIT')
+            return
+        }
+        console.log('submitForReview')
+        const url = 'http://127.0.0.1:8000/api/editbusiness/update_status/' 
+        const token = user['token']
+        const auth_str = 'Token '+token
+        const headers = new Headers()
+        headers.set('Authorization', auth_str)
+        headers.set('Content-Type', 'application/json')
+        const options = {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify({business_id:business.id, status:'PENDING'})
+        }
+        const fetchResult = await fetch(url, options)
+        const updatedResult = await fetchResult.json()
+        const errors = updatedResult['result']
+        if(errors.length === 0){
+            console.log('no errors')
+            alert('Update successful')
+        }
+    }
+  
+    const handleClick = async () =>{
+        await submitForReview()
+        refreshBusinesses()
+    }
+    
+    return (
     <div className='container'>
         <h2>Policy Delivery Confirmation</h2>
+        <div>
+            <Button
+                text = 'Submit for Review'
+                onClick={ handleClick} 
+            />
+        </div>
         <BusinessDetailsFP docName = ' ' business = {business} refreshBusinesses = {refreshBusinesses}  writeAccess = {hasWriteAccess} />
     </div>
   )
